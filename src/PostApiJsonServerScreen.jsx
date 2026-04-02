31b9341dfb8f654d1ef99fc2946a64d631b51929
backendstockyaari <backendstockyaari1@gmail.com>
2026-04-02 11:21:10 +0530

    add Post Api component

---
+++ src/PostApiJsonServerScreen.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react';
+import { Text, TextInput, View, Button, StyleSheet } from 'react-native';
+
+const PostApiComponent = () => {
+  const [name, setName] = useState();
+  const [age, setAge] = useState();
+  const [city, setCity] = useState();
+  const SaveData= async()=>
+  {
+    const url = "http://10.0.2.2:3000/users"
+    let result = await fetch(url,{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify({ 
+  name, 
+  age: Number(age), 
+  city 
+})
+
+    });
+    result = await result.json();
+if(result){
+  console.warn("Data Added")
+}
+    
+  }
+
+  return (
+    <View>
+      <TextInput value={name} placeholder="Enter Name" style={style.input} onChangeText={(text)=>setName(text)} />
+      <TextInput value={age} placeholder="Enter Age" style={style.input} keyboardType='numeric' onChangeText={(text)=>setAge(text)} />
+      <TextInput value={city} placeholder="Enter city" style={style.input} onChangeText={(text)=>setCity(text)} />
+      <Button title="Submit" onPress={SaveData} />
+    </View>
+  );
+};
+export default PostApiComponent;
+
+const style = StyleSheet.create({
+  input: {
+    borderRadius: 2,
+    borderColor: 'black',
+  },
+});
--- src/TabNavigationComponent.jsx
+++ src/TabNavigationComponent.jsx
@@ -2,7 +2,7 @@ import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import Homepage from "./Home";
 import FlatListComponent from './FlatListScreen';
 import ApiScreenComponent from './ApiScreen';
-
+import PostApiComponent from './PostApiJsonServerScreen';
 const Tab=createBottomTabNavigator();
 
 
@@ -15,7 +15,7 @@ return(
 <Tab.Screen name="Home" component={Homepage} options={{title:"Home"}}/>
 <Tab.Screen name="Flatlist" component={FlatListComponent} options={{title:"FlatList"}}/>
 <Tab.Screen name="ApiScreenComponent" component={ApiScreenComponent} options={{title:"Api Demo"}}/>
-
+<Tab.Screen name="PostApiComponent" component={PostApiComponent} option={{title:"Post API Demo"}} />
 </Tab.Navigator>
 
 // </NavigationContainer>
